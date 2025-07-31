4df6d42d824fbb0b27a81f38e3bd627c2744d20e
Yam Goz <yamgoz50@gmail.com>
2025-07-31 16:14:53 +0300

    clean routes

--- Core/Src/http/routers/post_router.c
+++ Core/Src/http/routers/post_router.c
@@ -16,11 +16,6 @@
 void POST_requests_router(struct mg_connection *c, struct mg_http_message *hm){
 	char response[256] = {0};
 
-	// if (mg_match(hm->uri, mg_str("/api/firmware_update/rtos_mg.bin"), NULL)) {
-	//   // add non blocking timer and send http reply at mg_ota_end?
-	//   handle_firmware_upload(c, hm, "rtos_mg.bin");
-	// }
-
 	if (mg_match(hm->uri, mg_str("/flash/settings/set"), NULL)) {
 
 		network_settings new_settings;  // put in the begining
